82f8f5ec27d26c9add28b056e185e25ac942f1de
slchen <shihlon.chen@gmail.com>
2016-04-21 18:33:55 +0800

    add predef header

---
+++ predef.h
@@ -0,0 +1,13 @@
+#ifndef __PREDEF_H
+#define __PREDEF_H
+
+#include "common/version.h"
+#include "common/assert.h"
+#include "arch/arch.h"
+#include "compiler/compiler.h"
+#include "os/os.h"
+#include "standard/stdc.h"
+#include "standard/stdcxx.h"
+#include "standard/stdcli.h"
+
+#endif
